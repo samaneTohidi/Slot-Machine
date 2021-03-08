c728a92d98d30e117aa614315625226557ae8356
samane Tohidi <s.tohidi39@gmail.com>
2021-03-08 17:11:26 +0330

    Lesson42 create the header

--- Slot Machine/Views/ContentView.swift
+++ Slot Machine/Views/ContentView.swift
@@ -7,15 +7,108 @@
 
 import SwiftUI
 
+// MARK: - PROPERTIES
 struct ContentView: View {
+    
+// MARK: - BODY
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       ZStack {
+        
+        // MARK: - BACKGROUND
+        LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+
+    
+        
+        // MARK: - INTERFACE
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5) {
+
+          LogoView()
+            
+          Spacer()
+            // MARK: - HEADER
+            // MARK: - SCORE
+            
+            HStack{
+                
+                HStack {
+                    Text("Your\nCoins".uppercased())
+                        .scoreLabelStyle()
+                        .multilineTextAlignment(.trailing)
+                        
+                Text("100")
+                    .scoreNumberStyle()
+                    .modifier(ScoreNumberModifier())
+            }
+                
+            .modifier(ScoreContainerModifier())
+                
+                
+               Spacer()
+                
+                HStack {
+                    Text("200")
+                        .scoreNumberStyle()
+                        .modifier(ScoreNumberModifier())
+                    
+                    Text("High\nScore".uppercased())
+                        .scoreLabelStyle()
+                        .multilineTextAlignment(.leading)
+                        
+            }
+                
+            .modifier(ScoreContainerModifier())
+                
+            }
+            
+            
+            // MARK: - SLOT MACHINE
+            // MARK: - FOOTER
+            
+            Spacer()
+        }
+        
+        // MARK: - BUTTONS
+        
+        .overlay(
+          // RESET
+          Button(action: {
+            print("Reset the game")
+          }) {
+            Image(systemName: "arrow.2.circlepath.circle")
+          }
+          .font(.title)
+          .accentColor(Color.white),
+//          .modifier(ButtonModifier()),
+          alignment: .topLeading
+        
+        )
+        .overlay(
+          // INFO
+          Button(action: {
+            print("Info View")
+          }) {
+            Image(systemName: "info.circle")
+          }
+          .font(.title)
+          .accentColor(Color.white),
+//          .modifier(ButtonModifier()),
+          alignment: .topTrailing
+        
+        )
+        
+        .padding()
+        .frame(maxWidth: 720)
+        
+        // MARK: POPUP
+        // ZStack
+        }
     }
 }
-
+ 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
