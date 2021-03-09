cc4787866154b5b037bc55475c2a061d73ea9f39
samane Tohidi <s.tohidi39@gmail.com>
2021-03-09 15:41:19 +0330

    Lesson48 game is over

--- Slot Machine/Views/ContentView.swift
+++ Slot Machine/Views/ContentView.swift
@@ -19,6 +19,9 @@ struct ContentView: View {
     @State private var showingInfoView: Bool = false
     @State private var isActiveBet10: Bool = true
     @State private var isActiveBet20: Bool = false
+    @State private var showingModal: Bool = false
+    @State private var animatingSymbol: Bool = false
+    @State private var animatingModal: Bool = false
     
     
     // MARK: - FUNCTIONS
@@ -75,6 +78,13 @@ struct ContentView: View {
       isActiveBet10 = true
       isActiveBet20 = false
       
+    }
+    
+    func isGameOver() {
+      if coins <= 0 {
+        showingModal = true
+       
+      }
     }
     
     
@@ -176,9 +186,13 @@ struct ContentView: View {
           // SPIN THE REELS
                     self.spinReels()
                     
-            // CHECK WINNIG
+          // CHECK WINNIG
                     
                     self.checkWinning()
+                    
+        // GAME IS OVER
+                    
+                    self.isGameOver()
                     
                 }){
                  Image("gfx-spin")
@@ -273,9 +287,75 @@ struct ContentView: View {
         
         .padding()
         .frame(maxWidth: 720)
+        .blur(radius: $showingModal.wrappedValue ? 5 : 0, opaque: false)
         
-        // MARK: POPUP
-        // ZStack
+        // MARK: - POPUP
+        if $showingModal.wrappedValue {
+          ZStack {
+            Color("ColorTransparentBlack").edgesIgnoringSafeArea(.all)
+            
+            // MODAL
+            VStack(spacing: 0) {
+              // TITLE
+              Text("GAME OVER")
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.heavy)
+                .padding()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .background(Color("ColorPink"))
+                .foregroundColor(Color.white)
+              
+              Spacer()
+              
+              // MESSAGE
+              VStack(alignment: .center, spacing: 16) {
+                Image("gfx-seven-reel")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(maxHeight: 72)
+                
+                Text("Bad luck! You lost all of the coins. \nLet's play again!")
+                  .font(.system(.body, design: .rounded))
+                  .lineLimit(2)
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(Color.gray)
+                  .layoutPriority(1)
+                
+                Button(action: {
+                  self.showingModal = false
+                  self.animatingModal = false
+                  self.activateBet10()
+                  self.coins = 100
+                }) {
+                  Text("New Game".uppercased())
+                    .font(.system(.body, design: .rounded))
+                    .fontWeight(.semibold)
+                    .accentColor(Color("ColorPink"))
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 8)
+                    .frame(minWidth: 128)
+                    .background(
+                      Capsule()
+                        .strokeBorder(lineWidth: 1.75)
+                        .foregroundColor(Color("ColorPink"))
+                    )
+                }
+              }
+              
+              Spacer()
+            }
+            .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+            .opacity($animatingModal.wrappedValue ? 1 : 0)
+            .offset(y: $animatingModal.wrappedValue ? 0 : -100)
+            .animation(Animation.spring(response: 0.6, dampingFraction: 1.0, blendDuration: 1.0))
+            .onAppear(perform: {
+              self.animatingModal = true
+            })
+          }
+        }
         }
        .sheet(isPresented: $showingInfoView){
         InfoView()
